c9ad111401c9ff2d2155a209002490b0442a8472
Lucas-Gabriel-dev <lucasgabriel7070@outlook.com>
2022-11-13 19:15:27 -0300

    Projeto concluido

---
+++ TesteTriangulo/CalculoTriangulo.cs
@@ -0,0 +1,45 @@
+﻿namespace TesteTriangulo;
+public class CalculoTriangulo
+    {
+        public int ResultadoTriangulo(string dadosTriangulo)
+        {
+            List<int> numbers = new List<int>();
+
+            foreach (var item in dadosTriangulo)
+            {
+                if (char.IsDigit(item))
+                {
+                    int digito = item - '0';
+
+                    numbers.Add(digito);
+                }
+            }
+
+            int valueTotal = numbers[0];
+            int firstValue = 0;
+            int secondValue = 0;
+            int count = 2;
+
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                firstValue = numbers[i];
+
+                if(i < numbers.Count - 1){
+                    secondValue = numbers[i + 1];
+                }
+
+                if(firstValue >= secondValue){
+                    valueTotal += firstValue;
+                }
+
+                if(firstValue < secondValue){
+                    valueTotal += secondValue;
+                }
+
+                i += count;
+                count += 1;
+            }
+
+            return valueTotal;
+        }
+    }
---
+++ TesteUnitario/TesteTriangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TesteTriangulo;
+
+namespace TesteCandidatoTDD
+{
+    [TestClass]
+    public class TesteTriangulo
+    {
+        [TestMethod]
+        public void TestResultadoTriangulo()
+        {
+            int retorno = new CalculoTriangulo().ResultadoTriangulo("[[6],[3,5],[9,7,1],[4,6,8,4]]");
+            Assert.IsTrue(retorno == 26);
+
+            retorno = new CalculoTriangulo().ResultadoTriangulo("[[6],[3,5],[9,7,1]]");
+            Assert.IsTrue(retorno == 18);
+
+            retorno = new CalculoTriangulo().ResultadoTriangulo("[[6],[3,5]]");
+            Assert.IsTrue(retorno == 11);
+
+            retorno = new CalculoTriangulo().ResultadoTriangulo("[[6],[3,5],[9,1,3],[4,6,1,4]]");
+            Assert.IsTrue(retorno == 18);
+
+            retorno = new CalculoTriangulo().ResultadoTriangulo("[[6],[3,5],[9,1,3],[4,6,6,4]]");
+            Assert.IsTrue(retorno == 20);
+
+            retorno = new CalculoTriangulo().ResultadoTriangulo("[[1],[1,1],[1,1,1],[1,1,1,1]]");
+            Assert.IsTrue(retorno == 4);
+
+            retorno = new CalculoTriangulo().ResultadoTriangulo("[[1],[1,1],[1,1,1]]");
+            Assert.IsTrue(retorno == 3);
+        }
+    }
+}
